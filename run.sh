63d3c055838c30a977b39234660153745e45356b
Pavel Yuruts <PavelYuruts@coherentsolutions.com>
2023-10-18 21:03:51 +0200

    updated run local script, fixed dockerfile, updated submodules

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 set -e;
 set +x;
 
-IMAGE_NAME="backend-api"
-CONTAINER_NAME="${IMAGE_NAME}-container"
+IMAGE_NAME="todolist-backend-api"
+APP_CONTAINER_NAME="${IMAGE_NAME}-container"
 MONGO_IMAGE_NAME="mongo"
 MONGO_CONTAINER_NAME="${MONGO_IMAGE_NAME}-container"
 DOCKER_NETWORK_NAME=todolist-network
@@ -18,13 +18,13 @@ MONGO_CONTAINER_IP="127.0.0.1"
 MONGO_CONTAINER_PORT="27017"
 MONGO_CONNECTION_URL="mongodb://${MONGO_USER}:${MONGO_PWD}@${MONGO_CONTAINER_IP}:${MONGO_CONTAINER_PORT}/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2"
 
-IS_RUNNING_SERVER=$(docker ps -qa --filter name=$CONTAINER_NAME)
+IS_RUNNING_SERVER=$(docker ps -qa --filter name=$APP_CONTAINER_NAME)
 IS_RUNNING_MONGO=$(docker ps -qa --filter name=$MONGO_CONTAINER_NAME)
 IS_NETWORK_CREATED=$(docker network ls -q --filter name=$DOCKER_NETWORK_NAME)
 
 if [[ -n $IS_RUNNING_SERVER ]]; then
-    echo -e "\033[1;31m Cleanup docker container...\033[0m";
-    docker stop $CONTAINER_NAME;
+    echo -e "\033[1;31m Cleanup app container...\033[0m";
+    docker stop $APP_CONTAINER_NAME;
 fi
 
 if [[ -n $IS_RUNNING_MONGO ]]; then
@@ -55,21 +55,24 @@ docker \
     $MONGO_IMAGE_NAME:latest \
     ;
 
-echo -e "\033[1;31m Starting build...\033[0m";
-cd server/app && bun dev;
-echo -e "\033[1;31m Starting ${CONTAINER_NAME}...\033[0m";
+echo -e "\033[1;31m Updating submodules...\033[0m";
+git submodule update --remote
+
+echo -e "\033[1;31m Starting api...\033[0m";
+cd server/todolist-api && bun install && bun dev;
+
+echo -e "\033[1;31m Starting ${APP_CONTAINER_NAME}...\033[0m";
 docker \
     run \
-    --name $CONTAINER_NAME \
+    --name $APP_CONTAINER_NAME \
     --rm \
     -p 3001:80 \
     --network $DOCKER_NETWORK_NAME \
     --env MONGO_CONNECTION_URL=$MONGO_CONNECTION_URL \
     -d \
-    -v /$(pwd)/app:/app/build \
+    -v /$(pwd)/server/todolist-api:/app/build \
     $IMAGE_NAME:latest \
     ;
 echo -e "\033[1;31m Starting watch...\033[0m";
-bun run --watch /app/src/index.ts;
 
 exit 0
